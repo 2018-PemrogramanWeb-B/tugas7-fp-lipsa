9be53f7b3efef09a634c0b0be835731d895ed307
heirsall <38243055+heirsall@users.noreply.github.com>
2018-11-06 23:04:05 +0700

    Add files via upload

---
+++ page-quiz.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>Page Pertanyaan</title>
+</head>
+<body>
+
+<div id="page-wrap">
+	
+	<h1>Jawab pertanyaan ini sesuai dengan keinginan anda</h1>
+
+	<form action="hasil.php" method="post" id="quiz">
+
+	<ol>
+		<li>
+
+		<h3>Ini pertanyaan 1?</h3>
+
+		<div>
+			<input type="radio" name="pertanyaan1" id="pertanyaan1-jawab-A" value="A"/>
+			<label for="pertanyaan1-jawab-A">Jawab nomer 1</label>
+		</div>
+
+		<div>
+			<input type="radio" name="pertanyaan1" id="pertanyaan1-jawab-B" value="B"/>
+			<label for="pertanyaan1-jawab-B">Jawab nomer 2</label>
+		</div>
+
+		<div>
+			<input type="radio" name="pertanyaan1" id="pertanyaan1-jawab-C" value="C"/>
+			<label for="pertanyaan1-jawab-C">Jawab nomer 3</label>
+		</div>
+
+		</li>
+
+		<li>
+		
+		<h3>Ini pertanyaan 2?</h3>
+
+		<div>
+			<input type="radio" name="pertanyaan2" id="pertanyaan2-jawab-A" value="A"/>
+			<label for="pertanyaan1-jawab-A">Jawab nomer 1</label>
+		</div>
+
+		<div>
+			<input type="radio" name="pertanyaan2" id="pertanyaan2-jawab-B" value="B"/>
+			<label for="pertanyaan1-jawab-B">Jawab nomer 2</label>
+		</div>
+
+		<div>
+			<input type="radio" name="pertanyaan2" id="pertanyaan2-jawab-C" value="C"/>
+			<label for="pertanyaan1-jawab-C">Jawab nomer 3</label>
+		</div>
+
+		</li>
+
+		<li>
+		
+		<h3>Ini pertanyaan 3?</h3>
+
+		<div>
+			<input type="radio" name="pertanyaan3" id="pertanyaan3-jawab-A" value="A"/>
+			<label for="pertanyaan1-jawab-A">Jawab nomer 1</label>
+		</div>
+
+		<div>
+			<input type="radio" name="pertanyaan3" id="pertanyaan3-jawab-B" value="B"/>
+			<label for="pertanyaan1-jawab-B">Jawab nomer 2</label>
+		</div>
+
+		<div>
+			<input type="radio" name="pertanyaan3" id="pertanyaan3-jawab-C" value="C"/>
+			<label for="pertanyaan1-jawab-C">Jawab nomer 3</label>
+		</div>
+
+		</li>
+	</ol>	
+
+	<input type="submit" value="Submit Pilhanmu">
+</form>
+
+</div>
+
+</body>
+</html>
