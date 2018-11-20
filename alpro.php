3a056e4b52e79dd9e6b87c8b0d3e97a39c466005
Bkhufa <42761825+BryanKhufa@users.noreply.github.com>
2018-11-20 19:51:41 +0700

    Add files via upload

---
+++ alpro.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html>
+<head lang="id">
+	<meta charset="utf-8">
+	<title>Alpro - Algoritma & Pemrograman</title>
+</head>
+<body>
+	<div class="navbar">
+		<a href="home" title="Home">Logo Website</a>
+		<a href="test" title="Take a Test">Take a Test</a>
+		<a href="daftar_lab.php" title="Detail Lab">Daftar Lab</a>
+	</div>
+	<br>
+	<h1>Alpro - Algoritma & Pemrograman</h1>
+	<div><img src="" alt="Logo Alpro"></div>
+	<div>
+		<div>
+			Sebagian besar mata kuliah dalam RMK ini adalah hal dasar yang harus diperhatikan dalam membuat software. Pada RMK ini kita mempelajari cara menyelesaikan permasalahan sehari-hari dan menerapkannya dengan menggunakan konsep pemrograman berorientasi objek. Cara mengoptimalkan program dengan memperhatikan kompleksitas. Kita juga akan mempelajari cara memanfaatkan dan memodifikasi framework sesuai kebutuhan kita. Kita juga dapat mempelajari pemrograman mobile.
+		</div>
+		<h2>Data :</h2>
+		<p>Jumlah Komputer	: </p>
+		<p>Jumlah Admin		: </p>
+		<p>Dosen 			: </p>
+		<p>Website 			: <a href = "alpro.if.its.ac.id"> alpro.if.its.ac.id </a> </p>
+		<div>
+			<p>RMK: </p>
+			<pre>Pemrograman Berorientasi Objek		-	Semester 3</pre>
+			<pre>Perancangan & Analisis Algoritma 	-	Semester 4</pre>
+			<pre>Pemrograman Web	-	Semester 5</pre>
+			<pre>Pemrograman Berbasis Kerangka Kerja			-	Semester 6</pre>
+			<pre>Pemrograman Perangkat Bergerak	-	Semester 7</pre>
+		</div>
+		<div>
+			<h2>Peluang Kerja :</h2>
+			<p>Mobile App Developer
+				<img src="" alt="Mobile App Developer">
+			</p>
+			<p>Algorithm Engineer
+				<img src="" alt="Algorithm Engineer">
+			</p>
+			<p>Web Developer
+				<img src="" alt="Web Developer">
+			</p>
+		</div>
+		<div>
+			<h2>Contoh TA :</h2>
+			<p>Rancangan Bangun Aplikasi NULIS: Media Pembelajaran Siswa Disgrafia - Peni Sriwahyu Natasari 2012 </p>
+			<p>Rancangan Bangun Sistem Pemetaan Alat Pembatas Kecepatan Menggunakan Android Smartphone dengan Akselerometer - Otniel Yehezkiel B. Hutabarat 2012 </p>
+		</div>
+		<div>
+			<img src="" alt="Foto-foto Lab Alpro">
+			<button>Previous</button>
+			<button>Next</button>
+		</div>	
+	</div>
+</body>
+</html>
---
+++ kcv.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head lang="id">
+	<meta charset="utf-8">
+	<title>KCV - Komputasi Cerdas & Visualisasi</title>
+</head>
+<body>
+	<div class="navbar">
+		<a href="home" title="Home">Logo Website</a>
+		<a href="test" title="Take a Test">Take a Test</a>
+		<a href="daftar_lab.php" title="Detail Lab">Daftar Lab</a>
+	</div>
+	<br>
+	<h1>KCV - Komputasi Cerdas & Visualisasi</h1>
+	<div><img src="" alt="Logo KCV"></div>
+	<div>
+		<div>
+			RMK berfokus pada membuat kecerdasan pada komputer. Kita akan mempelajari kecerdasan dalam komputer mulai dari cara belajar (mengumpulkan data), berfikir (mengolah data), dan memberikan tanggapan (hasil representasi dari data yang telah dikumpulkan dan diolah). Penerapan dari RMK ini ada bermacam-macam, contohnya di Robotika, Biomedika, Sistem rekomendasi, dan lain-lain.
+		</div>
+		<h2>Data :</h2>
+		<p>Jumlah Komputer	: </p>
+		<p>Jumlah Admin		: </p>
+		<p>Dosen 			: </p>
+		<p>Website 			: <a href = "kcv.if.its.ac.id"> kcv.if.its.ac.id </a> </p>
+		<div>
+			<p>RMK: </p>
+			<pre>Kecerdasan Buatan		-	Semester 4</pre>
+			<pre>Kecerdasan Komputasional	-	Semester 5</pre>
+			<pre>Analisis Data Multivariat	-	Semester 6</pre>
+			<pre>Pengolahan Citra Digital	-	Semester 6</pre>
+			<pre>Data Mining			-	Semester 7</pre>
+			<pre>Robotika			-	Semester 7</pre>
+			<pre>Sistem Temu Kembali Informasi	-	Semester 7</pre>
+			<pre>Visi Komputer			-	Semester 7</pre>
+		</div>
+		<div>
+			<h2>Peluang Kerja :</h2>
+			<p>Data Analyst
+				<img src="" alt="Data Analyst">
+			</p>
+			<p>Machine Learning Engineer
+				<img src="" alt="Machine Learning Engineer">
+			</p>
+			<p>AI Programmer
+				<img src="" alt="AI Programmer">
+			</p>
+		</div>
+		<div>
+			<h2>Contoh TA :</h2>
+			<p>Implementasi Artificial Neural Networks (ANN) untuk Memprediksi Jumlah Data Anomali Trafik Jaringan - Altea Sabila 2012</p>
+			<p>Deteksi Area Hijau Pada Daerah Urban Berbasis Metode Ekstrasi Oriented FAST and Rotated BRIEF (ORB) dari Citra Udara Dengan Resolusi Sangat Tinggi - Aziz Arijaya 2012</p>
+		</div>
+		<div>
+			<img src="" alt="Foto-foto Lab KCV">
+			<button>Previous</button>
+			<button>Next</button>
+		</div>	
+	</div>
+</body>
+</html>
